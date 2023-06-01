1f543705aa75360687fa1a083259dbee2de5d737
Leo <leo.mitchell@education.nsw.gov.au>
2023-06-01 12:13:00 +1000

    kj

--- Line Game.py
+++ Line Game.py
@@ -3,10 +3,10 @@ import random
 
 window = tk.Tk()
 window.title("Line Game")
-window.geometry('600x600')
+window.geometry('700x700')
 
-canvas_width = 600
-canvas_height = 600
+canvas_width = 700
+canvas_height = 700
 
 c = tk.Canvas(window, width=canvas_width, height=canvas_height, bg='white')
 c.pack()
@@ -16,22 +16,26 @@ dot_spacing = 100
 line_thickness = 12
 
 player_first = True
-win_condition = 8
+win_condition = 12
 game_over = False
 
 result_label = tk.Label(window, text='', font=('Arial', 20, 'bold'), bg='white')
 result_label.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
 
-player_count_label = tk.Label(window, text="Player's count: 0", font=('Arial', 16), bg='white')
+player_count_label = tk.Label(window, text="Player's count: 0", font=('Arial', 16, 'bold'), bg='white', fg='crimson')
 player_count_label.place(relx=0.1, rely=0.95, anchor=tk.SW)
 
+branch_counts = []  # Store branch counts
 
+start_x = (canvas_width - (4 * dot_spacing + 4 * dot_size)) // 2
+start_y = (canvas_height - (4 * dot_spacing + 4 * dot_size)) // 2
 
 def player_wins():
     global game_over
     print('Player wins!')
     game_over = True
     result_label.config(text='Game Over! Player Wins.')
+
 
 def computer_wins():
     global game_over
@@ -45,10 +49,10 @@ def player_turn(event):
         return
     line_id = event.widget.find_closest(event.x, event.y)[0]
     color = c.itemcget(line_id, 'fill')
-    
+
     if color == 'crimson':
         return
-    
+
     if color == 'lightseagreen':
         return
 
@@ -56,68 +60,64 @@ def player_turn(event):
         c.itemconfig(line_id, fill='crimson')
         update_player_count()
         computer_turn()
-    
+
     longest_line_length = count_longest_line()
     print("Longest line length:", longest_line_length)
 
-    
     if all_colored():
         print('There are no more moves.')
 
     if not check_win_condition(longest_line_length):
         return
-    
+
 
 def computer_turn():
     global game_over
     if game_over:
         return
-    
+
     white_lines = [line for line in lines if c.itemcget(line, 'fill') == 'white']
     if white_lines:
         line_id = random.choice(white_lines)
-        c.itemconfig(line_id, fill='lightseagreen')  
+        c.itemconfig(line_id, fill='lightseagreen')
     else:
         return
 
 
 def count_longest_line():
-    lengths = {}
-    for line_id in lines:
-        if c.itemcget(line_id, 'fill') == 'crimson':
-            visited = set()
-            length = counting_function(line_id, visited, 1, lengths)
-            lengths[line_id] = length
+    longest_line_length = 0
+    visited = set()
+    visited_branch = set()
 
-    longest_line_length = max(lengths.values(), default=0)
+    for line_id in lines:
+        if c.itemcget(line_id, 'fill') == 'crimson' and line_id not in visited:
+            touching_line_length = 0
+            branch_length = 0  # Added branch_length variable
+            stack = [(line_id, line_id)]  # Tuple: (current_line, prev_line)
+            branch_detected = False
+            while stack:
+                current_line, prev_line = stack.pop()
+                if current_line not in visited:
+                    visited.add(current_line)
+                    touching_line_length += 1
+                    touching_lines = get_touching_red_lines(current_line)
+                    valid_touching_lines = [line for line in touching_lines if line != prev_line]
+                    stack.extend([(line, current_line) for line in valid_touching_lines])
+
+                    current_branch = (current_line, prev_line)
+                    if len(valid_touching_lines) >= 2 and not branch_detected and current_branch not in visited_branch:  # Check for T-shape
+                        print("Branch detected!")
+                        visited_branch.add(current_branch)
+                        branch_detected = True
+
+            longest_line_length = max(longest_line_length, touching_line_length)
+            if branch_detected:
+                branch_counts.append(1)  # Add 1 to branch_counts for each detected branch
+                print_branch_counts()  # Print branch counts
+
     return longest_line_length
 
 
-def counting_function(line_id, visited, current_length, lengths, prev_line_id=None):
-    visited.add(line_id)
-
-    if line_id not in lengths or current_length > lengths[line_id]:
-        lengths[line_id] = current_length
-
-    touching_lines = get_touching_red_lines(line_id)
-    valid_touching_lines = []
-
-    for neighbor_id in touching_lines:
-        if neighbor_id not in visited and neighbor_id != prev_line_id:
-            valid_touching_lines.append(neighbor_id)
-
-    if not valid_touching_lines:
-        return current_length
-
-    max_length = current_length
-
-    for neighbor_id in valid_touching_lines:
-        new_visited = visited.copy()
-        new_length = counting_function(neighbor_id, new_visited, current_length + 1, lengths, line_id)
-        if new_length > max_length:
-            max_length = new_length
-
-    return max_length
 
 
 def get_touching_red_lines(line_id):
@@ -138,7 +138,7 @@ def all_colored():
         if color == 'white':
             return False
     return True
-    
+
 
 def check_win_condition(player_count):
     if player_count >= win_condition:
@@ -162,34 +162,55 @@ def update_player_count():
     player_count_label.config(text="Player's count: " + str(player_count))
 
 
-start_x = (canvas_width - (3 * dot_spacing + 4 * dot_size)) // 2
-start_y = (canvas_height - (3 * dot_spacing + 4 * dot_size)) // 2
+def print_branch_counts():
+    print("Branch Counts:")
+    for i, count in enumerate(branch_counts):
+        print("Branch {}: {}".format(i + 1, count))
+
+
 
 lines = []
-for row in range(4):
-    for col in range(3):
-        x = start_x + col * (dot_size + dot_spacing) + dot_size // 2
-        y = start_y + row * (dot_size + dot_spacing) + dot_size // 2
-        line = c.create_line(x, y, x + dot_spacing + dot_size, y, fill="white", width=line_thickness)
-        lines.append(line)
-        c.tag_bind(line, '<Button-1>', player_turn)
-
-for row in range(3):
-    for col in range(4):
-        x = start_x + col * (dot_size + dot_spacing) + dot_size // 2
-        y = start_y + row * (dot_size + dot_spacing) + dot_size // 2
-        line = c.create_line(x, y, x, y + dot_spacing + dot_size, fill="white", width=line_thickness)
-        lines.append(line)
-        c.tag_bind(line, '<Button-1>', player_turn)
-
 dots = []
-for row in range(4):
-    for col in range(4):
-        x = start_x + col * (dot_size + dot_spacing)
-        y = start_y + row * (dot_size + dot_spacing)
-        c.create_oval(x, y, x + dot_size, y + dot_size, fill="black")
 
-if not player_first:
-    computer_turn()
+def start_new_game():
+    global game_over
+    game_over = False
+    branch_counts.clear()  # Clear branch_counts list
+    result_label.config(text='')
+    player_count_label.config(text="Player's count: 0")
+    c.delete('all')
+    lines.clear()
+    dots.clear()
+
+    for row in range(5):
+        for col in range(4):
+            x = start_x + col * (dot_size + dot_spacing) + dot_size // 2
+            y = start_y + row * (dot_size + dot_spacing) + dot_size // 2
+            line = c.create_line(x, y, x + dot_spacing + dot_size, y, fill="white", width=line_thickness)
+            lines.append(line)
+            c.tag_bind(line, '<Button-1>', player_turn)
+
+    for row in range(4):
+        for col in range(5):
+            x = start_x + col * (dot_size + dot_spacing) + dot_size // 2
+            y = start_y + row * (dot_size + dot_spacing) + dot_size // 2
+            line = c.create_line(x, y, x, y + dot_spacing + dot_size, fill="white", width=line_thickness)
+            lines.append(line)
+            c.tag_bind(line, '<Button-1>', player_turn)
+
+    for row in range(5):
+        for col in range(5):
+            x = start_x + col * (dot_size + dot_spacing)
+            y = start_y + row * (dot_size + dot_spacing)
+            c.create_oval(x, y, x + dot_size, y + dot_size, fill="black")
+
+    if not player_first:
+        computer_turn()
+
+
+start_new_game()
+
+restart_button = tk.Button(window, text="Restart", font=('Arial', 16, 'bold'), bg='goldenrod', command=start_new_game)
+restart_button.place(relx=0.9, rely=0.95, anchor=tk.SE)
 
 window.mainloop()
